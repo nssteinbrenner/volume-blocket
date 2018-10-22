ae10076bc15013c53fecc3b22bc90058ae9844e8
nssteinbrenner <40495384+nssteinbrenner@users.noreply.github.com>
2018-10-22 18:31:42 +0000

    Add files via upload

--- volume-applet.py
+++ volume-applet.py
@@ -11,18 +11,24 @@ pulse = pulsectl.Pulse('headphones')
 
 parser = argparse.ArgumentParser(description="A pulseaudio control script")
 
-parser.add_argument('--raisevol','-r', default=False, type=float,
-        help="decimal value to raise volume (0.01-1.00)",required=False)
-parser.add_argument('--lowervol','-l', default=False, type=float,
-        help="decimal value to lower volume (0.01-1.00)",required=False)
+parser.add_argument('--raisevol', '-r', default=False, type=float,
+                    help="decimal value to raise volume (0.01-1.00)",
+                    required=False)
+parser.add_argument('--lowervol', '-l', default=False, type=float,
+                    help="decimal value to lower volume (0.01-1.00)",
+                    required=False)
 parser.add_argument('--mute', '-m', default=False, action='store_true',
-        help="toggle mute",required=False)
-parser.add_argument('--volume', '-v',default=False, action='store_true',
-        help="return current volume",required=False)
-parser.add_argument('--change','-c',default=False, action='store_true',
-        help="change audio output device",required=False)
-parser.add_argument('--printvol','-p',default=False, action='store_true',
-        help="print volume",required=False)
+                    help="toggle mute",
+                    required=False)
+parser.add_argument('--volume', '-v', default=False, action='store_true',
+                    help="return current volume",
+                    required=False)
+parser.add_argument('--change', '-c', default=False, action='store_true',
+                    help="change audio output device",
+                    required=False)
+parser.add_argument('--printvol', '-p', default=False, action='store_true',
+                    help="print volume",
+                    required=False)
 
 args = parser.parse_args()
 
@@ -36,13 +42,16 @@ icons = {
 with open('config', 'r') as f:
     headphones = f.readline().strip()
 
+
 def signalBar():
     call(['pkill', '-RTMIN+1', 'i3blocks'])
+
 
 def getSink(device):
     for sink in pulse.sink_list():
         if headphones in str(sink):
             return sink
+
 
 def raiseVolume(sink, vol=0.05):
     if checkMute(sink):
@@ -56,11 +65,13 @@ def raiseVolume(sink, vol=0.05):
         pulse.volume_change_all_chans(sink, +vol)
         signalBar()
 
+
 def lowerVolume(sink, vol=0.05):
     if checkMute(sink):
         unmute(sink)
     pulse.volume_change_all_chans(sink, -vol)
     signalBar()
+
 
 def checkMute(sink):
     index = str(sink).find('mute=')
@@ -70,20 +81,22 @@ def checkMute(sink):
     else:
         return True
 
+
 def mute(sink):
     pulse.mute(sink)
     signalBar()
 
+
 def unmute(sink):
     pulse.mute(sink, 0)
     signalBar()
+
 
 def showVolume(sink):
     if checkMute(sink):
         return icons['mute']
     else:
         visual = int(getVolume(sink)[:-1])//10
-        remainder = 10 - visual
         if visual <= 3:
             icon = icons['low']
         elif visual >= 4 and visual <= 6:
@@ -92,7 +105,7 @@ def showVolume(sink):
             icon = icons['high']
         show = icon + getVolume(sink)
         return show
-    
+
 
 def getVolume(sink):
     index = str(sink.volume).find('[')
@@ -104,6 +117,7 @@ def getVolume(sink):
         vol = '0.' + vol
         pulse.set_all_chans(sink, vol)
         return channels[0]
+
 
 sink = getSink(headphones)
 
